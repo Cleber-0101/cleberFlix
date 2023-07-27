42b17cd3af79be33515925d7901760cc001e409e
Cleber-esx <cleber.batista@esx.com.br>
2023-07-27 01:24:07 -0300

    Dando inicio ao projeto

---
+++ cleberFlix/src/main/java/cleberFlix/Condicionais.java
@@ -0,0 +1,27 @@
+package cleberFlix;
+
+public class Condicionais {
+
+	public static void main(String[] args) {
+		
+		int anoDeLancamento = 1999;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
+
+        if (anoDeLancamento >= 1999) {
+            System.out.println("Clientes gostam de assistir ! ");
+        } else if (anoDeLancamento <= 1999) {
+            System.out.println("Clientem não gostam de assistir ");
+        }
+
+
+        if (!incluidoNoPlano && !tipoPlano.equals("plus")) {
+            System.out.println("Deve primeiro pagar o plano para assistir");
+        } else {
+            System.out.println("Filme liberado !!!");
+        }
+
+	}
+
+}
