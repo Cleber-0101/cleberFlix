f1a32dceb2a999552dec84aa47299f9b2fa429be
Cleber-esx <cleber.batista@esx.com.br>
2023-07-27 21:57:29 -0300

    Criando uma simulação de conta bancaria

---
+++ DesafioContaBancaria/src/main/java/Desafio.java
@@ -0,0 +1,18 @@
+
+public class Desafio {
+
+	public static void main(String[] args) {
+		String nome = "Cleber Batista";
+		String tipoConta = "Corrente";
+		double saldo = 1200.00;
+		
+		
+		System.out.println("****************************");
+		System.out.println("\nNome do cliente: " + nome);
+		System.out.println("Tipo de conta: " + tipoConta);
+		System.out.println("saldo atual: " + saldo );
+		System.out.println("****************************");
+
+	}
+
+}
